fccfab9e63e255ed14a215a3505f2b0f6be5ee14
rockRayhan <khayrulalamdict@gmail.com>
2024-02-25 13:32:40 +0600

    login

--- resources/js/Pages/Login.jsx
+++ resources/js/Pages/Login.jsx
@@ -15,18 +15,20 @@ const Login = () => {
         <div>
            <Header/> 
             
-            <h1> Please Login </h1>
-<form className="space-y-6" method="POST" action="{{ route('student.login') }}">
+<div className='mt-5' style={{border:'2px solid black', marginLeft:'50px', marginTop:'50px'}}>
+<h1> Please Login </h1>
+<form className="space-y-6" onSubmit={submit} method="POST" action={ route('student.login') }>
 
         <div className='row my-3'>
-            <input type="text" name='email' placeholder='Enter Email'/>
+            <input type="text" name='email' value={data.email} placeholder='Enter Email' onChange={(e) => setData('email', e.target.value)}/>
         </div>
         <div className='row my-3'>
-            <input type="text" name='password' placeholder='Enter Password'/>
+            <input type="text" name='password' placeholder='Enter Password' value={data.password} onChange={(e) => setData('password', e.target.value)}/>
         </div>
 
 </form>
 
+</div>
 
            <Footer/>
         </div>
